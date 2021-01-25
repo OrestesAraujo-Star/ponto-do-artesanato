d2ddf9be91572bf94ecef031a36054e7adaf4dda
OrestesAraujo-Star <orestesaraujo@gmail.com>
2021-01-25 10:49:14 -0300

    Project created

---
+++ backend/src/main/java/com/gastech/pontodoartesanato/PontodoartesanatoApplication.java
@@ -0,0 +1,13 @@
+package com.gastech.pontodoartesanato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontodoartesanatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontodoartesanatoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/gastech/pontodoartesanato/PontodoartesanatoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gastech.pontodoartesanato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontodoartesanatoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
